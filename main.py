e2f03f861445b9c403d07a7f1823ce8409f070b8
AnkitPaswan <ankitpaswan192@gmail.com>
2026-06-09 20:51:41 +0530

    Remove age verification code and consolidate sum calculation into main.py

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-age = int(input("Enter your age: "))
-if age >= 18:
-    print("You Can Vote!")
-else:
-    print("You Cannot Vote!")
+# age = int(input("Enter your age: "))
+# if age >= 18:
+#     print("You Can Vote!")
+# else:
+#     print("You Cannot Vote!")
+
+
+num1 = input("Enter the first number: ")
+num2 = input("Enter the second number: ")
+sum = float(num1) + float(num2)
+print("The sum of", num1, "and", num2, "is", sum)
--- sum.py
+++
@@ -1,4 +0,0 @@
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-sum = float(num1) + float(num2)
-print("The sum of", num1, "and", num2, "is", sum)
