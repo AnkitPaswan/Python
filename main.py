b9acda76a5455731e83c10a6a32f24a6b34e8034
AnkitPaswan <ankitpaswan192@gmail.com>
2026-06-13 19:14:25 +0530

    Refactor sum function and comment out input code

--- main.py
+++ main.py
@@ -5,7 +5,13 @@
 #     print("You Cannot Vote!")
 
 
-num1 = input("Enter the first number: ")
-num2 = input("Enter the second number: ")
-sum = float(num1) + float(num2)
-print("The sum of", num1, "and", num2, "is", sum)
+# num1 = input("Enter the first number: ")
+# num2 = input("Enter the second number: ")
+# sum = float(num1) + float(num2)
+# print("The sum of", num1, "and", num2, "is", sum)
+
+# function
+def sum(num1,num2):
+    print("The sum of", num1, "and", num2, "is", num1 + num2);
+sum(10,40)
+    
