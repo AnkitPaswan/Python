a2971c7619689ffd651657b9364631d729fff92a
AnkitPaswan <ankitpaswan192@gmail.com>
2026-06-16 18:58:06 +0530

    Uncomment list processing code and add functionality to find positive and negative numbers

--- main.py
+++ main.py
@@ -16,8 +16,21 @@
 # sum(10,40)
 
 # List
-item = ["ankit", 10, 20,"Kumar"]
+# item = ["ankit", 10, 20,"Kumar"]
 # print(item)
-# for i in len(0,item):
-#     print(item)
-    
+# for i in range(len(item)):
+#     print(item[i])
+
+# Find negative and positive numbers in a list
+
+numbers = [10, -5, 20, -3, 15, -8]
+positive_numbers = []
+negative_numbers = []
+
+for num in numbers:
+    if num >= 0:
+        positive_numbers.append(num)
+    else:
+        negative_numbers.append(num)
+print("Positive numbers:", positive_numbers)
+print("Negative numbers:", negative_numbers)
